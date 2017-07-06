2a53333f1c418207676667b819eba96d439631b2
Caleb Martinez <contact@calebmartinez.com>
2017-07-06 00:17:28 -0400

    Added basic server structure to serve html templates with static files

---
+++ server.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+var templates = template.Must(template.ParseFiles("html/index.html", "html/about.html"))
+
+func render(w http.ResponseWriter, page string) {
+	err := templates.ExecuteTemplate(w, page+".html", nil)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func defaultHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r.URL.Path)
+	if r.URL.Path == "/" {
+		render(w, "index")
+		return
+	}
+	http.NotFound(w, r)
+	return
+}
+
+func aboutHandler(w http.ResponseWriter, r *http.Request) {
+	render(w, "about")
+}
+
+func main() {
+	http.HandleFunc("/", defaultHandler)
+	http.HandleFunc("/about", aboutHandler)
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	http.ListenAndServe(":8080", nil)
+}
