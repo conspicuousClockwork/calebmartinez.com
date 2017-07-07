3f23ddafc7b49ec1bcdffd53c1a5c76981ad4e27
Caleb Martinez <contact@calebmartinez.com>
2017-07-06 20:38:26 -0400

    Added contact and work routes

--- server.go
+++ server.go
@@ -6,7 +6,7 @@ import (
 	"net/http"
 )
 
-var templates = template.Must(template.ParseFiles("html/index.html", "html/about.html"))
+var templates = template.Must(template.ParseFiles("html/index.html", "html/about.html", "html/contact.html", "html/work.html"))
 
 func render(w http.ResponseWriter, page string) {
 	err := templates.ExecuteTemplate(w, page+".html", nil)
@@ -30,8 +30,19 @@ func aboutHandler(w http.ResponseWriter, r *http.Request) {
 	render(w, "about")
 }
 
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	render(w, "contact")
+}
+
+func workHandler(w http.ResponseWriter, r *http.Request) {
+	render(w, "work")
+}
+
 func init() {
 	http.HandleFunc("/", defaultHandler)
+	http.HandleFunc("/index", defaultHandler)
 	http.HandleFunc("/about", aboutHandler)
+	http.HandleFunc("/contact", contactHandler)
+	http.HandleFunc("/work", workHandler)
 	http.ListenAndServe(":8080", nil)
 }
